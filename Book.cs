1b00e805038841131f075c4bb0918a6783bb9782
cowqun <eminovcowqun96@gmail.com>
2024-04-01 15:54:58 +0400

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp65
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+        private string publisher;
+        private int availableCopies;
+
+        public Book(string title, string author, string publisher, int availableCopies)
+        {
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.availableCopies = availableCopies;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+
+        public int AvailableCopies
+        {
+            get { return availableCopies; }
+            set { availableCopies = value; }
+        }
+
+        public void IncreaseCopies(int amount)
+        {
+            availableCopies += amount;
+        }
+
+        public void DecreaseCopies(int amount)
+        {
+            availableCopies -= amount;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Kitabın adı: " + title);
+            Console.WriteLine("Müəllifi: " + author);
+            Console.WriteLine("Nəşriyyat: " + publisher);
+            Console.WriteLine("Mövcud nüsxələrin sayı: " + availableCopies);
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp65
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Kitabın Adı", "Müəllifin Adı", "Nəşriyyatın Adı", 10);
+            book.DisplayInfo();
+
+            Console.WriteLine("nNüsxələrin sayını artırmaq:");
+            book.IncreaseCopies(5);
+            book.DisplayInfo();
+
+            Console.WriteLine("nNüsxələrin sayını azaltmaq:");
+            book.DecreaseCopies(3);
+            book.DisplayInfo();
+        }
+  }
+    }
